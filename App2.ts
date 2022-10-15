b3d30f9c57021c90baca87fedce4cb005dadc248
Ariane <arianerochaf@gmail.com>
2022-10-15 11:28:18 -0300

    insert class Carro

---
+++ App2.ts
@@ -0,0 +1,36 @@
+class Carro {
+    private modelo: string
+    private numeroDePortas: number
+    private velocidade: number = 0
+
+    constructor(modelo: string, numeroDePortas: number) {
+        this.modelo = modelo
+        this.numeroDePortas = numeroDePortas
+    }
+    public acelerar(): void {
+        this.velocidade = this.velocidade + 10
+    }
+    public parar(): void {
+        this.velocidade = 0
+    }
+    public velocidadeAtual(): number {
+        return this.velocidade
+    }
+}
+
+class Concessionaria {
+    private endereco: string
+    private listaDeCarros: any //poderá receber qualquer dado
+
+    constructor(endereco: string) {
+        this.endereco = endereco
+    }
+
+    public fornecerEndereco(): string {
+        return this.endereco
+    }
+
+    public mostrarListaDeCarros(): any {
+        return this.listaDeCarros
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TypeScript
-TypeScript é um superste (superconjunto) de funcionalidades aplicadas a linguagem JavaScript que nos permite criar aplicações mais robustas e seguras.
+## TypeScript é um superste (superconjunto) de funcionalidades aplicadas a linguagem JavaScript que nos permite criar aplicações mais robustas e seguras.
